5ffbc01e440eeb805079bf19eafea126b38fce72
tshrrx <144604940+tshrrx@users.noreply.github.com>
2025-02-14 12:31:51 +0530

    mavenproject added

---
+++ shrinker-kit/src/main/java/com/shrinker_kit/shrinker_kit/ShrinkerKitApplication.java
@@ -0,0 +1,13 @@
+package com.shrinker_kit.shrinker_kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrinkerKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrinkerKitApplication.class, args);
+	}
+
+}
---
+++ shrinker-kit/src/test/java/com/shrinker_kit/shrinker_kit/ShrinkerKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrinker_kit.shrinker_kit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrinkerKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
